bd4f741f2c3c1499d211f30808a0278360fa9f99
DerMuffin <53898156+DerMuffin@users.noreply.github.com>
2022-12-12 18:46:09 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,14 +13,15 @@ gen64() {
 }
 install_3proxy() {
   echo "installing 3proxy"
-  wget "https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
-  tar -xvf 3proxy-0.8.6.tar.gz
-  cd 3proxy-3proxy-0.8.6
+  sudo apt install unzip
+  wget "https://github.com/3proxy/3proxy/releases/download/0.9.4/3proxy-0.9.4.zip"
+  unzip 3proxy-0.9.4.zip
+  cd 3proxy-3proxy-0.9.4
   make -f Makefile.Linux
   mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
   cp src/3proxy /usr/local/etc/3proxy/bin/
   cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-  chmod +x /etc/init.d/3proxy
+  sudo chmod +x /etc/init.d/3proxy
   chkconfig 3proxy on
   cd $WORKDIR
 }
